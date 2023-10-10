effe340d9b5acd7985aaa72ee0710b74dd5fdf40
PolinaShalak <palina.shalak@gmail.com>
2023-10-10 19:39:45 +0200

    Added initial settings

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="wrapper">
+    <HelloWorld msg="You did it!" />
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+@media (min-width: 1024px) {
+  .wrapper {
+    display: flex;
+  }
+}
+</style>
